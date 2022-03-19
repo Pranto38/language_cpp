f1dc109e2cc0bacc1a6ceebc7acd81bf81a3409b
Pranto <1902134@student.ruet.ac.bd>
2022-03-19 19:44:35 +0600

    Add files via upload

---
+++ Pattern/pattern1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,row,col;
+    cout<<"Enter the value of n: ";cin>>n;
+    for(row=1; row<=n; row++){
+        for(col=1; col<=row; col++){
+            cout<<"*"<<" ";
+        }
+    cout<<"\n";
+    }
+    for(row=n-1;row>=1;row--){
+        for(col=1; col<=row; col++){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
