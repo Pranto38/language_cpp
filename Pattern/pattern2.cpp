a4dad237da60fa9d42c9446c391cd4c426dbb8b5
Pranto <pmahamud1983@gmail.com>
2022-03-19 21:45:03 +0600

    Create pattern2.cpp

---
+++ Pattern/pattern2.cpp
@@ -0,0 +1,36 @@
+/*
+Enter the value of n: 4
+   *
+  **
+ ***
+****
+ ***
+  **
+   *
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,row,col;
+    cout<<"Enter the value of n: ";cin>>n;
+    for(row=1; row<=n; row++){
+        for(col=1; col<=n-row; col++){
+            cout<<" ";
+        }
+        for(col=1; col<=row; col++){
+            cout<<"*";
+        }
+    cout<<"\n";
+    }
+    for(row=n-1;row>=1;row--){
+        for(col=1; col<=n-row; col++){
+            cout<<" ";
+        }
+        for(col=1; col<=row; col++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
